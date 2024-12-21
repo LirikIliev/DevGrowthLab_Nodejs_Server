590d7c5d5f88d17e048a927b921df47be73ee09a
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-12-21 21:37:47 +0200

    update category  schema

--- src/models/category.ts
+++ src/models/category.ts
@@ -1,31 +1,45 @@
 import { model, Schema } from 'mongoose';
 import { URL_REGEX } from '../config/config';
 import { ERROR_MESSAGES } from './config';
+import { ICategory, ICategoryModel } from './modelTypes';
 
-const CategorySchema = new Schema({
+const CategorySchema = new Schema<ICategory>({
   name: {
     type: String,
-    require: [true, ERROR_MESSAGES.REQUIRE('Name')],
+    required: [true, ERROR_MESSAGES.REQUIRE('Name')],
     minLength: [3, ERROR_MESSAGES.MIN_LENGTH('three')],
   },
   image: {
     type: String,
-    require: [true, ERROR_MESSAGES.REQUIRE('Image')],
+    required: [true, ERROR_MESSAGES.REQUIRE('Image')],
     validate: {
       validator: function (image: string) {
         return URL_REGEX.test(image);
       },
       message: ERROR_MESSAGES.URL('image'),
     },
-    createAt: {
-      type: Date,
-      require: [true, ERROR_MESSAGES.CREATION_DATE],
-    },
-    updatedAt: {
-      type: Date,
-      default: new Date(),
-    },
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    default: new Date(),
+  },
+  admin: {
+    type: Schema.ObjectId,
+    ref: 'Users',
+    required: [true, ERROR_MESSAGES.REQUIRE('Admin id')],
   },
 });
 
-export default model('Category', CategorySchema);
+CategorySchema.statics.isCategoryExist = async function (
+  data
+): Promise<boolean> {
+  const category = await this.findOne(data);
+
+  return !!category;
+};
+
+export default model<ICategory, ICategoryModel>('Category', CategorySchema);
