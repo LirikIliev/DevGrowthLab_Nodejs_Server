bdde87ccdd39e484251b19e4e69dc7573c355dda
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-12-21 21:30:08 +0200

    add bcrypt salt

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
 export const SERVER_PORT = 4000;
 export const EMAIL_REGEX = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 export const URL_REGEX = /^https?:\/{2}/;
+export const BCRYPT_SALT = 12;
