19b52d2d2dd0644c7ca1c5b3ce154f221c6d737f
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-12-21 21:39:10 +0200

    create config file into controllers

---
+++ src/controllers/config.ts
@@ -0,0 +1,15 @@
+export const USER_FORM_KEYS = [
+  'email',
+  'password',
+  'name',
+  'role',
+  'avatar',
+] as const;
+export const PRODUCT_FORM_KEYS = [
+  'title',
+  'price',
+  'description',
+  'images',
+  'category',
+] as const;
+export const CATEGORY_FORM_KEYS = ['name', 'image', 'admin'] as const;
