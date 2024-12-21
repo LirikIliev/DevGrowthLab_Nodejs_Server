9cdf894c81b5e589ade1446cd7ffa0e66b75affc
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-12-21 21:33:45 +0200

    extract logic from product router file and set it into product service

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,77 +1,23 @@
-import { Router, Request, Response } from 'express';
-import { products } from '../testData/data';
-import { ProductInterface, ProductQueryType } from './types';
+import { Router } from 'express';
+
+import productHandlers from '../controllers/productServices';
+
 const router = Router();
-
-router.get('/list-of-products', (req: Request, res: Response) => {
-  res.json({ products });
-});
-
-router.get(
-  '/:productId/product-data',
-  (req: Request<ProductQueryType>, res: Response) => {
-    const productId = req.params.productId;
-    const productIndex = products.findIndex((pr) => pr.id === productId);
-
-    if (productIndex < 0) {
-      res.status(400).json({ message: 'The product info is missing!' });
-      return;
-    }
-
-    const product = products[productIndex];
-
-    res.status(200).json(product);
-  }
+// GET => get all products (list of them)
+router.get('/public/list-of-products', productHandlers.getListOfProducts);
+// GET => get product by id;
+router.get('/public/:productId/product-data', productHandlers.getProductById);
+// POST => add new product;
+router.post('/private/:userId/add-new-product', productHandlers.addNewProduct);
+// UPDATE => update product by id;
+router.put(
+  '/private/:userId/:productId/update-product',
+  productHandlers.updateProduct
 );
-
-router.put('/:productId/update-product', (req: Request, res: Response) => {
-  const productId = req.params.productId;
-  const body = req.body as ProductInterface;
-
-  if (!body.type || !body.name || !body.price) {
-    res.status(400).json({ message: 'The product info is missing' });
-    return;
-  }
-
-  const productIndex = products.findIndex((pr) => pr.id === productId);
-
-  if (productIndex < 0) {
-    res.status(400).json({ message: 'Product is missing' });
-    return;
-  }
-
-  const updatedProduct = {
-    id: products[productIndex].id,
-    name: body.name,
-    price: Number(body.price),
-    type: body.type,
-  };
-
-  products.splice(productIndex, 1, updatedProduct);
-
-  res.status(200).json({
-    message: 'The product was successfully updated!',
-    product: products[productIndex],
-  });
-});
-
+// DELETE => delete product by id;
 router.delete(
-  '/:productId/delete-product',
-  (req: Request<ProductQueryType>, res: Response) => {
-    const productId = req.params.productId;
-
-    const productIndex = products.findIndex((pr) => pr.id === productId);
-    if (productIndex < 0) {
-      res.status(400).json({ message: 'The product is missing!' });
-      return;
-    }
-
-    products.splice(productIndex, 1);
-
-    res
-      .status(200)
-      .json({ message: 'The product is successfully removed!', products });
-  }
+  '/private/:useId/:productId/delete-product',
+  productHandlers.removeProduct
 );
 
 export default router;
