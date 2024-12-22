8007cae8350ae321b9907c384e99a84ae65ba2cb
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-12-22 14:38:59 +0200

    create global constant for jwt info into config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,10 @@ export const SERVER_PORT = 4000;
 export const EMAIL_REGEX = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 export const URL_REGEX = /^https?:\/{2}/;
 export const BCRYPT_SALT = 12;
+export const JWT_OPTIONS = {
+  secret: '8a4b5c2d1f3e7g9h6j0k5n2p8r4q1z3x7y5v0w2t8u3s999s733',
+  expiresIn: '5h',
+};
 
 export const USERS_ROLE = {
   ADMIN: 'admin',
