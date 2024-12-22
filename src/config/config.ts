3cd9fc4c39d8fa84953f858886141de324332206
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-12-22 11:45:30 +0200

    extract predefined user roles into global config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,3 +2,8 @@ export const SERVER_PORT = 4000;
 export const EMAIL_REGEX = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 export const URL_REGEX = /^https?:\/{2}/;
 export const BCRYPT_SALT = 12;
+
+export const USERS_ROLE = {
+  ADMIN: 'admin',
+  GUEST: 'guest',
+};
