865da14518b7d25a5b46663dbb8c022af6f2018c
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-12-22 11:57:01 +0200

    remove unneeded comments into helpers.ts

--- src/routes/helpers/helpers.ts
+++ src/routes/helpers/helpers.ts
@@ -2,21 +2,20 @@ type ObjectDataType<T extends Record<string | number, any>> = T;
 type KeyType<T> = keyof T;
 
 export const hasEmptyFields = <T extends Record<string | number, any>>(
-  bodyKeys: readonly (keyof T)[], // Keys to check
+  bodyKeys: readonly (keyof T)[],
   body: ObjectDataType<T>
 ): boolean => {
   return bodyKeys.some((key) => {
     const value = body[key];
-    // Define "empty" conditions
     return (
       value === null ||
       value === undefined ||
-      (typeof value === 'string' && value.trim() === '') || // Empty string
-      (typeof value === 'number' && isNaN(value)) || // NaN check
-      (Array.isArray(value) && value.length === 0) || // Empty array
+      (typeof value === 'string' && value.trim() === '') ||
+      (typeof value === 'number' && isNaN(value)) ||
+      (Array.isArray(value) && value.length === 0) ||
       (typeof value === 'object' &&
         value !== null &&
-        Object.keys(value).length === 0) // Empty object
+        Object.keys(value).length === 0)
     );
   });
 };
