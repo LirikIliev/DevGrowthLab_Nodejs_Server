9a99f83ef015b386da2ef11475c4acf4d7139c39
LirikIliev <92252757+LirikIliev@users.noreply.github.com>
2024-12-23 00:25:25 +0200

    extract database success/fail message into config file

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,4 +2,9 @@ const DB_USER = 'kirilIvailovIliev:';
 const DB_PASS = 'dafhasdfl234234.alsdfkj';
 const DB_COLLECTION = 'shop-application';
 
+export const DATABASE_MESSAGES = {
+  SUCCESS: 'Database is successfully connected!',
+  FAIL: 'Unsuccessful connection with database!',
+};
+
 export const DATABASE_URL = `mongodb+srv://${DB_USER}${DB_PASS}@cluster0.mhqxh0n.mongodb.net/${DB_COLLECTION}?retryWrites=true&w=majority&appName=Cluster0`;
