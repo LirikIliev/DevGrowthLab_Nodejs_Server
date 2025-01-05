18015b5bab2310f050b070a8b73e08d0aae07a7f
k.iliev <kirilivailoviliev@gmail.com>
2025-01-05 14:05:04 +0200

    update general urls paths

--- src/routes/config.ts
+++ src/routes/config.ts
@@ -11,6 +11,15 @@ const paths = {
   ADD_PRODUCT: '/private/:productId/add-new-product',
   UPDATE_PRODUCT: '/private/:productId/update-product',
   REMOVE_PRODUCT: '/private/:productId/remove-product',
+  GET_BANNERS: '/public/banners',
+  ADD_BANNER: '/private/add-new-banner/',
+  REMOVE_BANNER: '/private/:bannerId/remove-banner',
+  GET_BRANDS: '/public/brands',
+  ADD_BRAND: '/private/add-new-brand',
+  REMOVE_BRAND: '/private/:brandId/remove-brand',
+  GET_BLOG_POSTS: '/public/blog-posts',
+  ADD_BLOG_POST: '/private/add-new-blog-post',
+  REMOVE_BLOG_POST: '/private/:postId/remove-blog-post',
 };
 
 export default paths;
