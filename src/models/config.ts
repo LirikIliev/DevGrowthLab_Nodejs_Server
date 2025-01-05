b964aed4bb1274c990c7963fbc396b72e2e45640
k.iliev <kirilivailoviliev@gmail.com>
2025-01-05 14:03:59 +0200

    add max length error message

--- src/models/config.ts
+++ src/models/config.ts
@@ -4,7 +4,10 @@ export const ERROR_MESSAGES = {
   CREATION_DATE: 'The creation date is required!',
   NAME: 'The name is required!',
   TITLE: 'Title is required',
-  MIN_LENGTH: (minLength: string) => `Min length is ${minLength} symbols!`,
+  MIN_LENGTH: (minLength: string | number) =>
+    `Min length is ${minLength} symbols!`,
+  MAX_LENGTH: (maxLength: string | number) =>
+    `Max length is ${maxLength} symbols!`,
   PRICE: 'Price must be positive!',
   REQUIRE: (reqValue: string) => `${reqValue} is/are required!`,
 };
