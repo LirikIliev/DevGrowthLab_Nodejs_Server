0fcc7a77c988fc435c2a9cff180ad48baf7a7824
k.iliev <kirilivailoviliev@gmail.com>
2025-01-31 20:32:52 +0200

    update model types

--- src/models/modelTypes.ts
+++ src/models/modelTypes.ts
@@ -26,6 +26,39 @@ export interface ICategory extends Document {
   updatedAt: Date;
 }
 
+interface Size {
+  size: string | number;
+  quantity: number;
+}
+interface Size {
+  size: string | number;
+  quantity: number;
+}
+
+export interface Product {
+  _id?: Types.ObjectId;
+  save(): unknown;
+  title: string;
+  brand: string;
+  type: string;
+  price: number;
+  gender: string;
+  images: string[];
+  sizes: Size[];
+  color: string;
+  colors: Types.ObjectId[];
+  category?: Types.ObjectId;
+  creationAt: Date;
+  updatedAt: Date;
+}
+
+export interface IProduct extends Model<Product> {
+  colorsUpdate: (
+    productId: Types.ObjectId,
+    productName: string
+  ) => Promise<void>;
+}
+
 type MongooseSearchingType = { [key: string | number]: string | number };
 export interface ICategoryModel extends Model<ICategory> {
   isCategoryExist: (data: MongooseSearchingType) => Promise<boolean>;
